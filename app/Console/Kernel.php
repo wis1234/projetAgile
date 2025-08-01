ccfea0d4e31b548753c9fec4d93084d8f3f30aca
wis1234 <ronaldoagbohou@gmail.com>
2025-08-01 05:13:41 +0100

    command for cron jobs on mailing

---
+++ app/Console/Kernel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // Exécute la file d'attente toutes les minutes
+        $schedule->command('queue:work --stop-when-empty')
+                 ->everyMinute()
+                 ->withoutOverlapping()
+                 ->sendOutputTo(storage_path('logs/queue-worker.log'));
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
